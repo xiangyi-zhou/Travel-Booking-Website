53b24eaf6690bfc0b6fb1287d3cce624b15dc28d
xiangyi-zhou <zxyzhouxiangyi@gmail.com>
2023-02-23 14:11:57 +0000

    update most styles

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,36 +1,30 @@
-import { Col, Container, Row } from "react-bootstrap";
-import { FaStreetView } from "react-icons/fa";
-import { GiDogHouse, GiJourney } from "react-icons/gi";
+import {Col, Container, Row} from "react-bootstrap";
+import {FaHotel, FaMapMarkerAlt, FaMountain} from "react-icons/fa";
 import classes from "../styles/Services.module.css";
 
 function Services() {
-  return (
-    <Container className={classes.service}>
-      <Row className={classes.row}>
-        <Col sm={12} md={12} lg={4} className="mt-4">
-          <GiJourney className={classes.icon} />
-          <h4 className="mt-4">Comfortable Journey</h4>
-          <p>
-            A wonderful serenity has taken to the possession of my entire soul.
-          </p>
-        </Col>
-        <Col sm={12} md={12} lg={4} className="mt-4">
-          <GiDogHouse className={classes.icon} />
-          <h4 className="mt-4">Luxuries Hotel</h4>
-          <p>
-            A wonderful serenity has taken to the possession of my entire soul.
-          </p>
-        </Col>
-        <Col sm={12} md={12} lg={4} className="mt-4">
-          <FaStreetView className={classes.icon} />
-          <h4 className="mt-4">Travel Guide</h4>
-          <p>
-            A wonderful serenity has taken to the possession of my entire soul.
-          </p>
-        </Col>
-      </Row>
-    </Container>
-  );
+    return (
+        <Container className={classes.service}>
+            <Row className={classes.row}>
+                <Col sm={12} md={12} lg={4} className="mt-4">
+                    <FaMountain className={classes.icon}/>
+                    <h4 className="mt-4">Scenic Tours</h4>
+                    <p>Explore breathtaking landscapes with our tours.</p>
+                </Col>
+                <Col sm={12} md={12} lg={4} className="mt-4">
+                    <FaHotel className={classes.icon}/>
+                    <h4 className="mt-4">Accommodations</h4>
+                    <p>Stay in style with luxury accommodations.</p>
+                </Col>
+                <Col sm={12} md={12} lg={4} className="mt-4">
+                    <FaMapMarkerAlt className={classes.icon}/>
+                    <h4 className="mt-4">Expert Guides</h4>
+                    <p>Discover hidden gems with expert guides.</p>
+                </Col>
+            </Row>
+
+        </Container>
+    );
 }
 
 export default Services;
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,74 +1,76 @@
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
-import { Component } from "react";
-import { Container } from "react-bootstrap";
-import { ImQuotesRight } from "react-icons/im";
+import {Component} from "react";
+import {Container} from "react-bootstrap";
+import {ImQuotesRight} from "react-icons/im";
 import OwlCarousel from "react-owl-carousel";
-import imgone from "../assets/images/Testmonial.webp";
+import testimonialImg1 from "../assets/images/Testmonial.webp";
+import testimonialImg2 from "../assets/images/Testmonial.webp";
+import testimonialImg3 from "../assets/images/Testmonial.webp";
 import "../styles/Testimonial.css";
 
 export class Owldemo1 extends Component {
-  render() {
-    return (
-      <div className="tesimonial">
-        <Container fluid className="container-fluidses">
-          <OwlCarousel
-            items={1}
-            className="owl-theme"
-            loop
-            autoplay={true}
-            margin={8}
-          >
-            <div className="cardss">
-              <img
-                className="imgases"
-                src={imgone}
-                alt="imgone"
-                style={{ width: "110px" }}
-              />
-              <ImQuotesRight className="quata" />
-              <p className="dummy mt-3 ">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s
-              </p>
-              <p className="dummy mt-2 text-muted">- Tom Mouse</p>
+    render() {
+        return (
+            <div className="tesimonial">
+                <Container fluid className="container-fluidses">
+                    <OwlCarousel
+                        items={1}
+                        className="owl-theme"
+                        loop
+                        autoplay={true}
+                        margin={8}
+                    >
+                        <div className="cardss">
+                            <img
+                                className="imgases"
+                                src={testimonialImg1}
+                                alt="testimonialImg1"
+                                style={{width: "110px"}}
+                            />
+                            <ImQuotesRight className="quata"/>
+                            <p className="dummy mt-3 ">
+                                Yichang's natural beauty left me speechless. The stunning landscapes and rich cultural
+                                heritage were truly unforgettable.
+                            </p>
+                            <p className="dummy mt-2 text-muted">- Sarah Lee</p>
+                        </div>
+
+                        <div className="cardss">
+                            <img
+                                className="imgases"
+                                src={testimonialImg2}
+                                alt="testimonialImg2"
+                                style={{width: "110px"}}
+                            />
+                            <ImQuotesRight className="quata"/>
+                            <p className="dummy mt-3 ">
+                                The luxury accommodations in Yichang were beyond my expectations. From the comfortable
+                                rooms to the world-class amenities, every detail was perfect.
+                            </p>
+                            <p className="dummy mt-2 text-muted">- John Smith</p>
+                        </div>
+
+                        <div className="cardss">
+                            <img
+                                className="imgases"
+                                src={testimonialImg3}
+                                alt="testimonialImg3"
+                                style={{width: "110px"}}
+                            />
+                            <ImQuotesRight className="quata"/>
+                            <p className="dummy mt-3 ">
+                                Yichang's expert guides helped me discover hidden gems and learn about the city's rich
+                                history and culture. I couldn't have asked for a better travel experience.
+                            </p>
+                            <p className="dummy mt-2 text-muted">- Rachel Chen</p>
+                        </div>
+
+                    </OwlCarousel>
+                </Container>
             </div>
-            <div className="cardss">
-              <img
-                className="imgases"
-                src={imgone}
-                alt="imgone"
-                style={{ width: "110px" }}
-              />
-              <ImQuotesRight className="quata" />
-              <p className="dummy mt-3 ">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s
-              </p>
-              <p className="dummy mt-2 text-muted">- Tom Mouse</p>
-            </div>
-            <div className="cardss">
-              <img
-                className="imgases position-relative"
-                src={imgone}
-                alt="imgone"
-                style={{ width: "110px" }}
-              />
-              <ImQuotesRight className="quata" />
-              <p className="dummy mt-3 ">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s
-              </p>
-              <p className="dummy mt-2 text-muted">- Tom Mouse</p>
-            </div>
-          </OwlCarousel>
-        </Container>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default Owldemo1;
